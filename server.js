4a8d137527dc8d829f11ecb9a561c6da97f4dc4c
GZHN <GZHN@live.com>
2017-03-23 13:13:14 +0100

    server update
    __dir added for macbook :(

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ fs = require("fs");
 path = require("path");
 
 server.listen(8000);
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 app.get('/', function(req,res){
-    res.sendfile('index.html');
+    res.sendfile(__dirname+'/index.html');
     });
 var io = require("socket.io").listen(server);
 console.log("Server running");
