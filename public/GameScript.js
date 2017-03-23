38a8d6d5674739cbb6dfcdaf2d8245419fe70a9e
Wesley <jemoeder42069@gmail.com>
2017-03-23 13:25:03 +0100

    Added color to circles

--- public/GameScript.js
+++ public/GameScript.js
@@ -14,24 +14,21 @@ canvasElement.appendTo('body');
 $("#submit").click(function() {
     var data = {
         name : $("#name").val(),
-        color : "roze"
+        color : $("#color").val()
     };
-    alert("winegums zijn de shit");
     socket.emit("join", data);
 });
 
 socket.on("update", function(gamestate){
-    alert("macbooks zijn stom >:(");
     draw(gamestate);
 })
 
 //draw function
 var draw = function(gamestate){
     gamestate.Players.forEach(function(index, key){
-        alert("test - " + index.name);
         canvas.beginPath();
         canvas.arc(index.x, index.y, 25, 0, Math.PI*2);
-        canvas.fillStyle = color;
+        canvas.fillStyle = index.color;
         canvas.fill();
     });
     
